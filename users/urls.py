59316bf99923a9fffa2d6060ba1c255618692674
bhargavi1poyekar <bhargavi1poyekar@gmail.com>
2020-09-05 23:52:35 +0530

    static files added

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from . import views as users_view
 
 urlpatterns = [
     path('register/',users_view.register,name='users-register'),
-    path('',users_view)
+    # path('',users_view.home,name='home'),
 ]
