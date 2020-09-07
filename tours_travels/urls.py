226680e75cc6842552c4e1598e6cc3a43f961a56
bhargavi1poyekar <bhargavi1poyekar@gmail.com>
2020-09-07 15:32:00 +0530

    Login Registration template added

--- tours_travels/urls.py
+++ tours_travels/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('users/',include('users.urls')),
     
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/index.html'),name='logout'),
 
 ]
 
--- users/forms.py
+++ users/forms.py
@@ -4,13 +4,15 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+
 class UserRegisterForm(UserCreationForm):
-	username = forms.CharField(label='Your name')
-	first_name = forms.CharField(help_text='First name')
-	last_name=forms.CharField()
-	email=forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Email'}) ,help_text='Enter a valid email address') 
-	password1=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}),label='password')
-	password2=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'confirm password'}),label='confirm password')
+
+	username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username','class':'input-box'}),label='')
+	first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'First Name','class':'input-box'}),label='')
+	last_name=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Last Name','class':'input-box'}),label='')
+	email=forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Email','class':'input-box'}),label='') 
+	password1=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password','class':'input-box'}),label='')
+	password2=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Confirm password','class':'input-box'}),label='')
 	# phone = forms.CharField(help_text='Phone number')
 	class Meta(UserCreationForm.Meta):
 		model = User
